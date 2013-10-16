0cc714073babb76cc61040779bf77e3ddcf6049e
Ben Ronne <ben.ronne@buzzfeed.com>
2013-10-16 13:48:00 -0400

    final madlib

--- madlib.rb
+++ madlib.rb
@@ -1,14 +1,36 @@
 puts "Welcome to Ruby Madlibs!\n"
+
 puts "Let's start with a name of someone you know."
 name = gets.chomp
+
 puts "Please enter a noun (a person, place or thing)."
 noun_1 = gets.chomp
-puts "Thanks!  Now, please enter a verb (that ends in 'ing')."
-verb_1 = gets.chomp
-puts "Nice work!  How about an adverb (something that ends in 'ly'."
+
+puts "Thanks!  How about an adverb (something that ends in 'ly'."
 adverb = gets.chomp
-puts "Almost done!  Give me one more noun..."
+
+puts "Nice work!  Now, please enter a verb (that ends in 'ing')."
+verb_1 = gets.chomp
+
+puts "Great.  Now, enter another noun."
 noun_2 = gets.chomp
-puts "And one more verb (in the past tense)..."
+
+puts "Now, enter another verb (in the past tense)"
 verb_2 = gets.chomp
-puts "Once upon a #{noun_1}, #{name} was #{adverb} #{verb_1} when they were interupted by a #{noun_2} that had just #{verb_2}"
+
+puts "And enter one more noun..."
+noun_3 = gets.chomp
+
+puts "Almost done!  Enter one more verb (in the past tense)..."
+verb_3 = gets.chomp
+
+puts "One last adjective..."
+adjective = gets.chomp
+
+puts "And one last verb (in the past tense)."
+verb_4 = gets.chomp
+
+puts "Generating your madlib..."
+sleep (3)
+puts "Here it is!"
+puts "Once upon a #{noun_1}, #{name} was #{adverb} #{verb_1} on their homework.\nSuddenly, a #{noun_2} burst through the door and #{verb_3} all over the #{noun_3}.  #{name} was so #{adjective} that they #{verb_4}."
